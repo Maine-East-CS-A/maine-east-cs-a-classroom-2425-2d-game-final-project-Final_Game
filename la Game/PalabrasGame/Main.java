a0305815bf8e7c464e88623a055e4d04732fa8b5
ray1yawn <shussain2@s207.org>
2025-05-15 15:12:49 +0000

    update game files

---
+++ la Game/PalabrasGame/Main.java
@@ -0,0 +1,10 @@
+import TsStuff.Palabras;
+
+public class Main {
+    public static void main(String[] args) {
+     
+        Palabras palabrasGame = new Palabras();
+        palabrasGame.startLaJuego();
+        palabrasGame.playLaJuego();
+    }
+}
